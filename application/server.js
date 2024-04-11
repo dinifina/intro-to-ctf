31474c35d0423bb22f7ec50ea4fb24960ca203ca
dinifina <ardinikamrula@gmail.com>
2024-04-11 21:05:33 +1000

    updated .gitignore and moving onto ejs

---
+++ application/server.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// using ejs
+app.set('view engine', 'ejs');
+
+//routes
+
+app.use("/users", require("./routes/auth"));
+
+app.listen(PORT, () => {
+    console.log(`Connected to port ${PORT}`);
+})
